26daebf65747a9ae44035872386d8789eeb91f8d
Moriah-Howard <107278295+Moriah-Howard@users.noreply.github.com>
2022-06-25 15:24:50 -0400

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,37 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast.
+# 2. A complete list of candidates who received votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+
+# import csv
+# dir(csv)
+
+# #Assign a variable for the file to load and the path.
+# file_to_load = "Resources/election_results.csv"
+# #Open the election results and read the file.
+# with open(file_to_load) as election_data:
+
+# #To Do: perofrm analysis
+
+# #Close the file.
+#     print (election_data)
+
+import csv
+import os
+from wsgiref import headers
+file_to_load = os.path.join("Resources", "election_results.csv")
+# with open(file_to_load) as election_data:
+#     print(election_data)
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# with open(file_to_save, "w") as txt_file:
+#     txt_file.write("Arapahoe,\n Denver, \n Jefferson")
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    # for row in file_reader:
+    #     print(row)
+
+
---
+++ Python_practice.py
@@ -0,0 +1,34 @@
+# print ("Hello World")
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+# print (counties[2])
+# counties.append("El Paso")
+# print (counties)
+# counties_dict = {}
+# counties_dict["Arapahoe"] = 422829
+# print (counties_dict)
+# counties_dict["Denver"] = 463353
+# counties_dict["Jefferson"] = 432438
+# print (counties_dict)
+# counties_dict.items()
+# voting_data = []
+# voting_data.append({"county":"Arapahoe", "registered_voters":422829})
+# voting_data.append({"county":"Denver", "registered_voters":463353})
+# voting_data.append({"county":"Jefferson", "registered_voters":432438})
+# print (voting_data)
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+# temperature = int(input("What is the termperature outside? "))
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+counties = ["Arapahoe", "Denver", "Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not in the list of counties.")
+if "Araphaoe" in counties and "El Paso" not in counties:
+    print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El PAso is not in the list of counties.")
